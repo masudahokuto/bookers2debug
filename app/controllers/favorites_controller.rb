c1a35056c2bc521a6976d0003dae03f42b39a712
EC2 Default User <ec2-user@ip-172-31-1-163.ap-northeast-1.compute.internal>
2024-07-24 08:23:23 +0000

    [finish]bookers2-debug-p2-7-a

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,21 +1,15 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    @book = book
-    respond_to do |format|
-      format.js
-    end
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    @book = book
-    respond_to do |format|
-      format.js
-    end
   end
 end
